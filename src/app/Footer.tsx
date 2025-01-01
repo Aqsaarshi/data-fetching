5d444328a00dea9b07fc15b65deb434adf0c604d
Aqsa Arshad <160477492+Aqsaarshi@users.noreply.github.com>
2025-01-01 19:48:50 +0500

    Update Footer.tsx

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="bg-purple-400 text-white py-6">
       <div className="container mx-auto text-center px-4">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} My Website. All rights reserved.
+          &copy; {new Date().getFullYear()} AqsaAura. All rights reserved.
         </p>
         <div className="flex justify-center space-x-4 mt-4">
           <a
